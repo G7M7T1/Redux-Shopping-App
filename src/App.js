2655f00e12864f5d224291abf2a4d8841ad1b4e0
G7M7T1 <genlisense@gmail.com>
2022-03-21 05:17:28 -0400

    使用redux toolkit之前的代码

--- src/App.js
+++ src/App.js
@@ -14,12 +14,17 @@ function App() {
     dispatch({type: 'DEC'})
   }
 
+  const addby = () => {
+    dispatch({type: 'ADD', payload: 10})
+  }
+
   return (
     <div>
       <h1>Counter App</h1>
       <p>{counter}</p>
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
+      <button onClick={addby}>Add Value 10</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,12 @@ const reducerFunction = (state = {counter: 10}, action) => {
     if (action.type === "DEC") {
         return {counter: state.counter - 1}
     }
+
+    if (action.type === "ADD") {
+        return {counter: state.counter + action.payload}
+        console.log(action)
+    }
+
     return state
 }
 
