3ab3b650fc101de4bbf628cf79a86e196559cc13
G7M7T1 <genlisense@gmail.com>
2022-03-21 05:44:19 -0400

    获取新的Starter File
    获取新的Starter File, 然后开始组建购物车的模块

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import './App.css';
-import {useSelector, useDispatch} from "react-redux";
-import {actions} from "./store";
+import React from "react";
+import "./App.css";
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
 
 function App() {
-  const counter = useSelector((state) => state.counter)
-
-  const dispatch = useDispatch()
-
-  const increment = () => {
-    dispatch(actions.increment(1))
-  }
-
-  const decrement = () => {
-    dispatch(actions.decrement(1))
-  }
-
-  const addValue = () => {
-    dispatch(actions.addValue(10))
-  }
-
   return (
-    <div>
-      <h1>Counter App</h1>
-      <p>{counter}</p>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
-      <button onClick={addValue}>Add Value 10</button>
+    <div className="App">
+      <Auth />
+      {/* <Layout /> */}
     </div>
   );
 }
